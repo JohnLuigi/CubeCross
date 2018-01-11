efa71bb41b125b499fd7c6c9af42dc5159ee7613
Shmiddy <eric.rackear@gmail.com>
2018-01-11 01:48:19 -0500

    Simple Cube Array Added
    Made a basic method for a single line of cubes to be created.

---
+++ CubeCross/Assets/Scripts/CubeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour {
+
+	public int puzzleSize = 10;
+	public GameObject[] cubeArray;
+	public GameObject exampleCube;
+
+	// create the array of cubes that will make up the puzzle
+	void Start () {
+		
+		createCubes ();
+	}
+
+	void Update () {
+		
+	}
+
+	// initialize the cube array
+	void createCubes()
+	{
+		// set a starting point based on the number of cubes to be created
+		// this starting point should be half of the number of cubes
+		Vector3 startingPoint = new Vector3((float) (puzzleSize / 2.0f) * -1.0f, 0, 0);
+
+		cubeArray = new GameObject[puzzleSize];
+
+		for (int i = 0; i < puzzleSize; i++) {
+			
+			GameObject newCube = Instantiate (exampleCube, startingPoint, Quaternion.identity) as GameObject;
+			newCube.transform.localScale = Vector3.one;
+			cubeArray [i] = newCube;
+
+			startingPoint += new Vector3 (1.0f, 0, 0);
+		}
+	}
+}
