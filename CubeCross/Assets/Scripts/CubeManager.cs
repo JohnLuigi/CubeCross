8e9285eff3b66ec24ccab735de19b2d6830d52d5
Shmiddy <eric.rackear@gmail.com>
2018-01-11 19:40:05 -0500

    Added detection and deletion of cubes and their types
    Can delete blank cubes, can be warned if you click on a solution cube.

--- CubeCross/Assets/Scripts/CubeManager.cs
+++ CubeCross/Assets/Scripts/CubeManager.cs
@@ -16,7 +16,40 @@ public class CubeManager : MonoBehaviour {
 
 	void Update () {
 		
+        // cube check/deletion block
+        // if the player left clicks once, see if a cube is hit
+        if(Input.GetMouseButtonDown(0))
+        {
+            checkCube();
+        }
+
 	}
+
+    // cast a ray at the location of the mouse click
+    void checkCube()
+    {
+        RaycastHit hitPoint;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hitPoint, Mathf.Infinity))
+        {
+            // if we hit a cube that is part of the solution, punish the player
+            if(hitPoint.collider.tag == "KeyCube")
+            {
+                Debug.Log("This cube is cube needs to stay.");
+            }
+
+            // if we hit a cube that is not part of the solution, "delete" the cube
+            else if(hitPoint.collider.tag == "BlankCube")
+            {
+                // TODO
+                // make an animation for deleting a blank cube
+                hitPoint.transform.gameObject.SetActive(false);
+            }
+        }
+
+
+    }
 
 	// initialize the cube array
 	void createCubes()
