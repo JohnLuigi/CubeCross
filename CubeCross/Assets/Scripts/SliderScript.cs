7e9aa9555f44681156f2b00bebc44939da47ef7e
Shmiddy <eric.rackear@gmail.com>
2018-01-31 20:03:44 -0500

    The player can now reset the slider position
    If the player doubleclicks on the slider, it goes back to its starting
    position. Still need to implement hiding of cubes based on silder
    distance from puzzle center.

--- CubeCross/Assets/Scripts/SliderScript.cs
+++ CubeCross/Assets/Scripts/SliderScript.cs
@@ -11,11 +11,16 @@ public class SliderScript : MonoBehaviour {
     public GameObject yReference;
     //public Vector3 yReference;
     public Plane movePlane;
+    public GameObject originalLocation;
 
     public Vector3 pointOnPlane;
     public Vector3 oldPoint;   // distance to be used to see how far the slider was dragged along a plane in a frame
     public float distanceToTravel;
     public bool unclicked = true;
+
+    private float clickTime = 0f;
+    private float timeTracker = 0f;
+    private float doubleClickDelay = 0.2f;  // default double click is half a second on windows
 
 
 	// Use this for initialization
@@ -26,9 +31,14 @@ public class SliderScript : MonoBehaviour {
         // set the starting point to be moved towards when dragging away from
         yReference = new GameObject();
         yReference.transform.position = new Vector3(0, 1, 0);
+        yReference.transform.parent = parentObject.transform;
+
         movePlane = new Plane(transform.parent.position, transform.position, yReference.transform.position);
 
-        yReference.transform.parent = parentObject.transform;
+        // store a gameobject that is parented to the gameManager cube puzzle
+        originalLocation = new GameObject();
+        originalLocation.transform.position = transform.position;
+        originalLocation.transform.parent = parentObject.transform;
 
         // record the original distance from the center of the puzzle (the parent)
         // this wil be the maximum distance the slider can be away from the puzzle
@@ -46,6 +56,22 @@ public class SliderScript : MonoBehaviour {
         {
             unclicked = true;
         }
+
+        timeTracker += Time.deltaTime;
+/*
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if(hit.transform.gameObject.tag == "Slider")
+                {
+                    // we've hit the slider, now lets find its location on the plane
+                }
+            }
+        }
+*/
     }
 
     // make the distance to travel be the same as the mouse horizontal traversal distance from the
@@ -56,6 +82,24 @@ public class SliderScript : MonoBehaviour {
         // make sure the slider has a parent, in this case the GameManager
         if (transform.parent != null)
         {
+            // double click code to reset the position of the mouse
+            if(Input.GetMouseButtonUp(0))
+            {
+                float oldClickTime = clickTime;
+
+                clickTime = timeTracker;
+
+                float timeSinceLastClick = clickTime - oldClickTime;
+
+                // if the last click was also on the cube
+                if(timeSinceLastClick <= doubleClickDelay && timeSinceLastClick != 0f)
+                {
+                    //reset the position of the slider
+                    transform.position = originalLocation.transform.position;
+                    return;
+                }
+            }
+
             // if the left mouse button is held over the slider controller
             if(Input.GetMouseButton(0))
             {
@@ -92,18 +136,20 @@ public class SliderScript : MonoBehaviour {
                     // Opposite vectors will be -1 and orthogonal vectors will be 0
 
                     float step = travelDirection.magnitude;
+                    float modifier = 1.0f;
                     // if the directions are in the same direction
                     // MOVE TOWARDS THE CUBES
                     if(1.0f - cosAngle <= 0.0009 )
                     {
                         //cosAngle is practically 1, so it is towards the cube
-                        step *= -1.0f;
+                        step *= -modifier;
                     }
                     // if the directions are away from each other
                     // MOVE AWAY FROM THE CUBES
                     else if(1.0 - cosAngle >= 1.99)
                     {
                         // cosAngle is practically -1, so it is away from the cube
+                        step *= modifier;
                     }
 
                     // if moving towards the center of the puzzle
@@ -119,9 +165,9 @@ public class SliderScript : MonoBehaviour {
                     if (currentDistance.magnitude > startingDistance.magnitude)
                     {
                         transform.position = Vector3.MoveTowards(transform.position, parentObject.transform.position,
-                    step * -1.0f);
+                    -step);
                         Debug.Log("too far");
-                        return;
+                        //return;
                     }
 
                     // save the point after translating the slider
